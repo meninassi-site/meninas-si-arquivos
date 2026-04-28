a22d051296af01df2967a3570cd842739a3862c7
Leandro Coutinho <leandrocoutinho.dev@gmail.com>
2026-04-28 08:24:51 -0300

    Delete front-end/meninasdesistema-front-main directory

--- front-end/meninasdesistema-front-main/src/components/Footer/footer.js
+++
@@ -1,7 +0,0 @@
-async function loadFooter() {
-  const response = await fetch("../components/footer/footer.html");
-  const data = await response.text();
-  document.getElementById("footer").innerHTML = data;
-}
-
-loadFooter();
